e5ae2ab8676190690bc7fca024d4579d4a2c2c8e
Kay Shinonome <kayshinonome@protonmail.com>
2021-12-03 14:05:30 -0600

    Various changes

---
+++ lib/ksnutils/include/future.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <core/ksnucleus.hpp>
+#include <types.hpp>
+
+template <typename WRAPPED_TYPE> class Future
+{
+  private:
+    WRAPPED_TYPE data;
+    bool has_been_called_yet = false;
+
+    WRAPPED_TYPE &fetch_data()
+    {
+        if (!has_been_called_yet)
+        {
+            if (callback == nullptr)
+            {
+                ks_fission("Future missing its callback");
+            }
+            data = callback();
+        }
+        return data;
+    }
+
+  public:
+    WRAPPED_TYPE (*callback)() = nullptr;
+
+    operator const WRAPPED_TYPE() const
+    {
+        return fetch_data();
+    }
+
+    operator WRAPPED_TYPE &()
+    {
+        return fetch_data();
+    }
+};
